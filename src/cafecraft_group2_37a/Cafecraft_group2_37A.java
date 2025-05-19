0b1e125612d9f2b15c89877b224a2b453c445c27
smarika <mesmarika7@gmail.com>
2025-05-19 11:22:55 +0545

    added new project

---
+++ src/cafecraft_group2_37a/Cafecraft_group2_37A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cafecraft_group2_37a;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cafecraft_group2_37A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
