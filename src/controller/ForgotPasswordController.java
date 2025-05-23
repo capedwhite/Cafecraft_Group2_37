b22d4cd9da97de341fb91926116081627d0ffe19
Unish <unishkc99@gmail.com>
2025-05-23 11:46:03 +0545

    added forget password and stopped database tracking

---
+++ src/controller/ForgotPasswordController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+import dao.ForgotPasswordDAO;
+import model.User;
+
+/**
+ *
+ * @author Unish K.C
+ */
+public class ForgotPasswordController {
+        public void resetPassword(String username, String newPassword) {
+        User user = new User(username, newPassword); // User object
+        ForgotPasswordDAO dao = new ForgotPasswordDAO();
+        dao.updatePassword(user);
+        }
+}
+
+
+
+
+
--- src/model/User.java
+++ src/model/User.java
@@ -10,4 +10,37 @@ package model;
  */
 public class User {
     
+    private String username;
+    private String password;
+
+    // Constructor (empty)
+    public User() {
+    }
+
+    // Constructor (with username and password)
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter for username
+    public String getUsername() {
+        return username;
+    }
+
+    // Setter for username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter for password
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
